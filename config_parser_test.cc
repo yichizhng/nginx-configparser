4ad5e620de0174fb0ca39483c75c96ea28657d4f
Yichi Zhang <yichizhng@gmail.com>
2014-04-18 02:10:09 -0700

    Added a test to see whether comments are properly ignored

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -18,3 +18,15 @@ TEST(NginxConfigParserTest, NestedBraces) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigParserTest, CommentTest) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+  
+  bool success = parser.Parse("testconfig2", &out_config);
+
+  EXPECT_TRUE(success);
+
+  EXPECT_STREQ("foo \"bar\";\nserver {\n  listen 80;\n  server_name foo.com;\n  root /home/ubuntu/sites/foo/;\n}\n", out_config.ToString(0).c_str());
+}
+
